2325a25c9567016e0fff9aa7d7072e5ef78f31c9
Vane737 <vivian_vanessa_flores@hotmail.com>
2023-09-05 17:18:42 -0400

    Procedimiento de busqueda

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+
+  searchByCapital( termino: string ): void {
+    console.log('Desde ByCapitalPage');
+    console.log( { termino });
+  }
 }
