7e61c708fd7ff2dfb7442a61f8a775399ef3eb14
Vane737 <77525733+Vane737@users.noreply.github.com>
2023-09-04 23:31:21 -0600

    routing

---
+++ src/app/shared/pages/contact-page/contact-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-contact-page',
+  templateUrl: './contact-page.component.html',
+  styleUrls: [
+
+  ]
+})
+export class ContactPageComponent {
+
+}
